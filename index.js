e4022b84b879974586109c515019136d5da50682
Futa IWATA <futaiwata@gmail.com>
2021-04-25 14:09:20 +0900

    added pg

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const Discord = require('discord.js');
+const express = require('express')
+const path = require('path')
 require('dotenv').config();
+
 const client = new Discord.Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
+
+const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
