34829d616d348a50585b5bffad59274763610506
Futa IWATA <futaiwata@gmail.com>
2021-04-25 15:07:57 +0900

    client -> dclient

--- index.js
+++ index.js
@@ -3,43 +3,17 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config();
 
-const client = new Discord.Client();
+const dclient = new Discord.Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+dclient.on('ready', () => {
+    console.log(`Logged in as ${dclient.user.tag}!`);
 });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
+dclient.on('message', msg => {
     if (msg.content === 'ping') {
         msg.reply('Pong!');
     }
 });
 
-client.login(BOT_TOKEN);
+dclient.login(BOT_TOKEN);
