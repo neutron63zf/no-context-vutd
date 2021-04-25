5ccf07117fe1c461d7f67c84f47ce15f84b9b72a
Futa IWATA <futaiwata@gmail.com>
2021-04-25 16:34:19 +0900

    configure postgresql

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const Discord = require('discord.js');
-const express = require('express')
 const path = require('path')
 require('dotenv').config();
 
 const dclient = new Discord.Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-dclient.on('ready', () => {
-    console.log(`Logged in as ${dclient.user.tag}!`);
+const express = require("express");
+const app = express();
+const db = require("./models/index");
+
+app.get("/", function (req, res) {
+  db.data.findAll({}).then((e) => {
+    res.send(e);
+  });
 });
 
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+dclient.on('ready', () => console.log(`Logged in as ${dclient.user.tag}!`));
+
 dclient.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+  if (msg.channel.name == 'no-context-vutd') {
+    if (msg.deleted){
+      db.data.findOne({
+        where: { url: msg.url }
+      }).then(item => {
+        item.destroy();
+      });
+    }else{
+      db.data.findOrCreate({
+        where: { url: msg.url },
+        defaults: {
+          url: msg.url,
+          content: msg.edits[-1],
+          isnew: true
+        }
+      }).then(([item, created]) => {
+        if(!created) {
+          item.content = msg.edits[-1];
+          item.save();
+        }
+      });
     }
+  }
 });
 
 dclient.login(BOT_TOKEN);
