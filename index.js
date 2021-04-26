fa0a5ef4741eec0312441800cd430c3c310c14dc
Futa IWATA <futaiwata@gmail.com>
2021-04-26 22:41:40 +0900

    added twit

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ require('dotenv').config();
 const dclient = new Discord.Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const cid = process.env.CHANNEL_ID;
+const tck = process.env.TWITTER_CONSUMER_KEY;
+const tcs = process.env.TWITTER_CONSUMER_SECRET;
+const tat  = process.env.TWITTER_ACCESS_TOKEN;
+const tts = process.env.TWOTTER_TOKEN_SECRET;
 
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000
 
 const db = require("./models/index");
+
+const T = new Twit({
+  consumer_key: tck,
+  consumer_secret: tcs,
+  access_token: tat,
+  access_token_secret: tts,
+  timeout_ms: 10*1000,
+  strictSSL: true
+})
 
 app.get("/", function (req, res) {
   db.data.findAll({}).then((e) => {
