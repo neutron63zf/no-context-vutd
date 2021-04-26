529b4524ba07e89a6390f5036627f9229588d354
Futa IWATA <futaiwata@gmail.com>
2021-04-26 21:28:24 +0900

    added passport-twitter

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const Discord = require('discord.js');
-const path = require('path')
 require('dotenv').config();
 
 const dclient = new Discord.Client();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const cid = process.env.CHANNEL_ID;
+const tck = process.env.TWITTER_CONSUMER_KEY;
+const tcs = process.env.TWITTER_CONSUMER_SECRET;
 
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000
 
 const db = require("./models/index");
+
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter');
+app.use(passport.session());
+
+passport.use(new TwitterStrategy({
+  consumerKey: tck,
+  consumerSecret: TWITTER_CONSUMER_SECRET,
+  callbackURL: 'http://no-context-vutd.herokuapp.com:' + port + '/auth/twitter/callback'
+},
+function(token, tokenSecret, profile, done) {
+  console.log(token)
+  console.log(tokenSecret)
+  return done(null,profile);
+}));
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', {
+        failureRedirect: '/' }),
+    function(req, res) {
+        res.redirect('/');
+    });
 
 app.get("/", function (req, res) {
   db.data.findAll({}).then((e) => {
